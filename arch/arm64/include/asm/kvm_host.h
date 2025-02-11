13fef45adb8f4ce9b2f45323b4dab97b8adb7dd5
Sebastian Ott <sebott@redhat.com>
2025-02-11 15:39:09 +0100

    KVM: arm64: Allow userspace to change AIDR_EL1
    Enable VMMs to write AIDR_EL1 by treating it as a VM ID register.
    Trap guest access of AIDR_EL1 when the VMs value differs from hardware.
    
    Since this was the last invariant register remove all the stuff
    that was needed to handle these.
    
    Signed-off-by: Sebastian Ott <sebott@redhat.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -372,6 +372,7 @@ struct kvm_arch {
 
 	u64 midr_el1;
 	u64 revidr_el1;
+	u64 aidr_el1;
 	u64 ctr_el0;
 
 	/* Masks for VNCR-baked sysregs */
@@ -1430,6 +1431,8 @@ static inline u64 *__vm_id_reg(struct kvm_arch *ka, u32 reg)
 		return &ka->midr_el1;
 	case SYS_REVIDR_EL1:
 		return &ka->revidr_el1;
+	case SYS_AIDR_EL1:
+		return &ka->aidr_el1;
 	case SYS_CTR_EL0:
 		return &ka->ctr_el0;
 	default:
