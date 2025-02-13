e87b8855c91c515e7f693dcc35fe18deff111b53
Yonggang Luo <luoyonggang@gmail.com>
2025-02-14 00:54:31 +0800

    add env.sh and build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+# git clean -xdf
+git reset --hard lygstate/main
+make oldconfig
+mv .config .config.new
+git commit -m "TEMP: Remove config from index" .config
+mv .config.new .config
+make deb-pkg -j16 || make deb-pkg -j16
